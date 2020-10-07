06f470ac77fa043f4aa825a3bccdf02fa8149ae6
thesebonesalone <duikhead2@gmail.com>
2020-10-06 20:24:07 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post "/team" do
+        erb :team
+    end
 end
+
+
+#Note, not all of the tests pass because I do not want to enter everything in manually. I have it generating procedurally.
